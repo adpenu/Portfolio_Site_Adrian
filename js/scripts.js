0a37dd8cda2861715e8c1667a3f6ffb208f758f4
Adrian Penu <ovidiu_penu@yahoo.com>
2022-10-02 12:29:29 +0100

    delete transition script

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,15 @@ if (x.className === 'navtoggle') {
   x.className = 'navtoggle';
 }
 }
+
+//*transition of burgr menu function menuToggle() {
+//  var x = document.getElementById('myNavtoggle');
+//  var z = document.getElementById('nav');
+//  if (x.className === 'navtoggle') {
+  //  x.className += ' responsive';
+  //    z.style.transition = "all 0.5s ease";
+//  } else {
+  //  x.className = 'navtoggle';
+  //      z.style.transition = "all 0.5s ease";
+//  }
+//}
